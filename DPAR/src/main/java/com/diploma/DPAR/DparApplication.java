ea2ec9b30c1901522cc349a7a3ea3e5e578a1bfc
WhyNotAG <golubkin1998@mail.ru>
2021-04-18 13:31:37 +0300

    add project settings

---
+++ DPAR/src/main/java/com/diploma/DPAR/DparApplication.java
@@ -0,0 +1,13 @@
+package com.diploma.DPAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DparApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DparApplication.class, args);
+	}
+
+}
---
+++ DPAR/src/test/java/com/diploma/DPAR/DparApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diploma.DPAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DparApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
